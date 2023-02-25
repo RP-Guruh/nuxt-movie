d959d046542e67741307f98fdc02679385ef8c13
rp-guruh <github.gurh@gmail.com>
2023-02-25 20:00:06 +0700

    import font and create layout slider group

---
+++ layouts/default.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-layout>
+        <v-app-bar :elevation="1" border="0" color="#191a26" density="compact" prominent>
+            <template v-slot:prepend>
+                <img src="@/assets/icons/netflix.png">
+            </template>
+            <template v-slot:append>
+
+                <v-app-bar-nav-icon color="white" class="pr-3" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+                <img class="rounded-full" src="https://i.pravatar.cc/35">
+            </template>
+        </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" location="bottom" temporary>
+            <v-list :items="items"></v-list>
+        </v-navigation-drawer>
+
+        <slot />
+
+        <v-bottom-navigation :elevation="2" grow dark>
+            <v-btn value="recent">
+                <v-icon>mdi-history</v-icon>
+                Recent
+            </v-btn>
+
+            <v-btn value="favorites">
+                <v-icon>mdi-heart</v-icon>
+                Favorites
+            </v-btn>
+
+            <v-btn value="nearby">
+                <v-icon>mdi-map-marker</v-icon>
+                Nearby
+            </v-btn>
+        </v-bottom-navigation>
+    </v-layout>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        drawer: false,
+        group: null,
+        items: [
+            {
+                title: 'Foo',
+                value: 'foo',
+            },
+            {
+                title: 'Bar',
+                value: 'bar',
+            },
+            {
+                title: 'Fizz',
+                value: 'fizz',
+            },
+            {
+                title: 'Buzz',
+                value: 'buzz',
+            },
+        ],
+    }),
+
+    watch: {
+        group() {
+            this.drawer = false
+        },
+    },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,105 +1,65 @@
 <template>
-    <div>
-        <v-layout>
-            <v-app-bar :elevation="1" border="0" color="#191a26" density="compact" prominent>
-                <template v-slot:prepend>
-                    <img src="@/assets/icons/netflix.png">
-                </template>
-                <template v-slot:append>
+  <NuxtLayout>
+    <v-main>
+      <v-container fluid>
 
-                    <v-app-bar-nav-icon color="white" class="pr-3" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-                    <img class="rounded-full" src="https://i.pravatar.cc/35">
-                </template>
-            </v-app-bar>
+        <div class="justify-start mb-4 select-none flex -ml-3">
+          <NuxtLink to="#"
+            class="focus:bg-indigo-600 focus:font-semibold focus:text-white focus:rounded-full py-2 px-4 no-underline font-bold text-sm border-blue btn-primary hover:text-white hover:bg-blue-light focus:outline-none active:shadow-none mr-2">
+            All</NuxtLink>
+          <NuxtLink to="#"
+            class="focus:bg-indigo-600 focus:font-semibold focus:text-white focus:rounded-full py-2 px-4 no-underline font-bold text-sm border-blue btn-primary hover:text-white hover:bg-blue-light focus:outline-none active:shadow-none mr-2">
+            Movie</NuxtLink>
+          <NuxtLink to="#"
+            class="focus:bg-indigo-600 focus:font-semibold focus:text-white focus:rounded-full py-2 px-4 no-underline font-bold text-sm border-blue btn-primary hover:text-white hover:bg-blue-light focus:outline-none active:shadow-none mr-2">
+            Series</NuxtLink>
+        </div>
 
-            <v-navigation-drawer v-model="drawer" location="bottom" temporary>
-                <v-list :items="items"></v-list>
-            </v-navigation-drawer>
+        <!-- TRENDING NOW -->
+        <div>
+          <span class="font-semibold text-lg text-white tracking-wide">Trending Now</span>
+        </div>
 
-            <v-main>
-                <v-container fluid>
+       
+          <v-slide-group v-model="model" class="" selected-class="bg-success">
+            <v-slide-group-item v-for="n in 15" :key="n" v-slot="{ isSelected, toggle, selectedClass }">
+              <v-card color="red" :class="['ma-4', selectedClass]" height="200" width="100" @click="toggle">
+                <div class="d-flex fill-height align-center justify-center">
+                  <v-scale-transition>
+                    <v-icon v-if="isSelected" color="white" size="48" icon="mdi-close-circle-outline"></v-icon>
+                  </v-scale-transition>
+                </div>
+              </v-card>
+            </v-slide-group-item>
+          </v-slide-group>
+       
 
-                    <div class="justify-start mb-16 select-none flex">
-                        <a href="#"
-                            class="focus:bg-indigo-600 focus:font-thin focus:text-white focus:rounded-full py-2 px-4 no-underline font-semibold text-sm border-blue btn-primary hover:text-white hover:bg-blue-light focus:outline-none active:shadow-none mr-2">All</a>
-                        <a href="#"
-                            class="focus:bg-indigo-600 focus:font-thin focus:text-white focus:rounded-full py-2 px-4 no-underline font-semibold text-sm border-blue btn-primary hover:text-white hover:bg-blue-light focus:outline-none active:shadow-none mr-2">Movie</a>
-                        <a href="#"
-                            class="focus:bg-indigo-600 focus:font-thin focus:text-white focus:rounded-full py-2 px-4 no-underline font-semibold text-sm border-blue btn-primary hover:text-white hover:bg-blue-light focus:outline-none active:shadow-none mr-2">Series</a>
-                        
-                    </div>
-
-
-
-                    <v-row dense>
-                        <v-col v-for="n in 8" :key="n" cols="4">
-                            <v-sheet color="grey-lighten-2" height="96"></v-sheet>
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </v-main>
-
-
-            <v-bottom-navigation :elevation="2" grow dark>
-                <v-btn value="recent">
-                    <v-icon>mdi-history</v-icon>
-
-                    Recent
-                </v-btn>
-
-                <v-btn value="favorites">
-                    <v-icon>mdi-heart</v-icon>
-
-                    Favorites
-                </v-btn>
-
-                <v-btn value="nearby">
-                    <v-icon>mdi-map-marker</v-icon>
-
-                    Nearby
-                </v-btn>
-            </v-bottom-navigation>
-        </v-layout>
-
-    </div>
+        <v-row dense class="mt-4">
+          <v-col v-for="n in 8" :key="n" cols="4">
+            <v-sheet color="grey-lighten-2" height="96"></v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </NuxtLayout>
 </template>
 
 <style>
 a {
-    color: #696e82;
-    font-family: 'Poppins', sans-serif;
+  color: #696e82;
+  font-family: 'Segoe UI', sans-serif;
+}
+
+span {
+  font-family: 'Poppins', sans-serif;
 }
 </style>
 
 <script>
   export default {
     data: () => ({
-      drawer: false,
-      group: null,
-      items: [
-        {
-          title: 'Foo',
-          value: 'foo',
-        },
-        {
-          title: 'Bar',
-          value: 'bar',
-        },
-        {
-          title: 'Fizz',
-          value: 'fizz',
-        },
-        {
-          title: 'Buzz',
-          value: 'buzz',
-        },
-      ],
+      model: null,
     }),
-
-    watch: {
-      group () {
-        this.drawer = false
-      },
-    },
   }
 </script>
+
